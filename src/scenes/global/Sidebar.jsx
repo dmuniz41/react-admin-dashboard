eebf335d8483152b187594e90ca6ed996074cf9f
Daniel <danielmuniz902@gmail.com>
2023-02-03 13:38:54 +0100

    finishing sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -140,6 +140,13 @@ const Sidebar = () => {
             selected={selected}
             setSelected={setSelected}
           />
+          <Typography
+            variant="h6"
+            color={colors.grey[300]}
+            sx={{ m: "15px 0 5px 20px" }}
+          >
+            Data
+          </Typography>
           <Item
             title="Manage Team"
             path="/team"
@@ -161,6 +168,13 @@ const Sidebar = () => {
             selected={selected}
             setSelected={setSelected}
           />
+          <Typography
+            variant="h6"
+            color={colors.grey[300]}
+            sx={{ m: "15px 0 5px 20px" }}
+          >
+            Pages
+          </Typography>
           <Item
             title="Profile Form"
             path="/form"
@@ -182,6 +196,13 @@ const Sidebar = () => {
             selected={selected}
             setSelected={setSelected}
           />
+          <Typography
+            variant="h6"
+            color={colors.grey[300]}
+            sx={{ m: "15px 0 5px 20px" }}
+          >
+            Charts
+          </Typography>
           <Item
             title="Bar Chart"
             path="/bar"
